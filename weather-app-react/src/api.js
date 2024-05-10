716fd2f5b8fb202862517b6ad023ba72dd03aed1
BHarris02 <118061899+BHarris02@users.noreply.github.com>
2024-05-10 21:38:08 +0100

    Initial Code

---
+++ weather-app-react/src/api.js
@@ -0,0 +1,12 @@
+export const optionsGeoAPI = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'efe75856e4msh0dc8fcd83193502p12902cjsne9fa07decd29',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '451b94a0d2833394b983edd0b3e9fe79'
+
